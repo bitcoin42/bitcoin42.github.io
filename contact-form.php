98772d0cd4f209b7f8564f7adcb2541b523b647b
bitcoin42 <alec@bitcoin42.com>
2018-04-02 15:48:54 -0700

    final?

--- contact-form.php
+++ contact-form.php
@@ -4,7 +4,7 @@
 //////////////////////////
 
 //Your E-mail
-$your_email = 'your@email.com';
+$your_email = 'contact@bitcoin42.com.mx';
 
 //Default Subject if 'subject' field not specified
 $default_subject = 'From My Contact Form';
